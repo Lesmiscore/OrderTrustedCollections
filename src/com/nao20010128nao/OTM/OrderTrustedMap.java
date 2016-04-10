8e4a98379881d0bbf96ec6105e0957effa9e3efe
nao20010128nao <nao20010128@gmail.com>
2016-04-10 23:35:36 +0900

    最初

---
+++ src/com/nao20010128nao/OTM/OrderTrustedMap.java
@@ -0,0 +1,102 @@
+package com.nao20010128nao.OTM;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class OrderTrustedMap<K, V> implements Map<K, V> {
+	Node nodes;
+	int curModified = 0;
+
+	public OrderTrustedMap() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	@Override
+	public int size() {
+		// TODO 自動生成されたメソッド・スタブ
+		if (isEmpty())
+			return 0;
+		Node curNode = nodes;
+		int count = 1;
+		while ((curNode = curNode.next) != null) {
+			count++;
+		}
+		return count;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO 自動生成されたメソッド・スタブ
+		return nodes == null;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO 自動生成されたメソッド・スタブ
+		if (isEmpty())
+			return false;
+
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO 自動生成されたメソッド・スタブ
+		if (isEmpty())
+			return false;
+	}
+
+	@Override
+	public V get(Object key) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public V put(K key, V value) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public V remove(Object key) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> m) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void clear() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public Set<K> keySet() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public Collection<V> values() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public Set<Map.Entry<K, V>> entrySet() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	class Node {
+		Node next;
+		K k;
+		V v;
+	}
+}

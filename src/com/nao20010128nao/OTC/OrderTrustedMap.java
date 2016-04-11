b7d73d108b3a1a2815242d6c48c0c04ac48fa1b9
nao20010128nao <nao20010128@gmail.com>
2016-04-11 18:28:29 +0900

    実装

--- src/com/nao20010128nao/OTC/OrderTrustedMap.java
+++ src/com/nao20010128nao/OTC/OrderTrustedMap.java
@@ -114,7 +114,7 @@ public class OrderTrustedMap<K, V> implements Map<K, V> {
 	@Override
 	public V put(K key, V value) {
 		// TODO 自動生成されたメソッド・スタブ
-		if (!containsKey(key))
+		if (containsKey(key))
 			return null;
 		Node curNode = nodes;
 		while ((curNode = curNode.next) != null) {
@@ -201,13 +201,13 @@ public class OrderTrustedMap<K, V> implements Map<K, V> {
 	@Override
 	public Set<K> keySet() {
 		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return new KeySet();
 	}
 
 	@Override
 	public Collection<V> values() {
 		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return new Values();
 	}
 
 	@Override
@@ -228,6 +228,29 @@ public class OrderTrustedMap<K, V> implements Map<K, V> {
 		Node next;
 		K k;
 		V v;
+
+		class ImmutableEntry implements Map.Entry<K, V> {
+			K lK = k;
+			V lV = v;
+
+			@Override
+			public K getKey() {
+				// TODO 自動生成されたメソッド・スタブ
+				return lK;
+			}
+
+			@Override
+			public V getValue() {
+				// TODO 自動生成されたメソッド・スタブ
+				return lV;
+			}
+
+			@Override
+			public V setValue(V value) {
+				// TODO 自動生成されたメソッド・スタブ
+				throw new UnsupportedOperationException();
+			}
+		}
 	}
 
 	abstract class Check {
@@ -267,21 +290,32 @@ public class OrderTrustedMap<K, V> implements Map<K, V> {
 		public Iterator<K> iterator() {
 			// TODO 自動生成されたメソッド・スタブ
 			check();
-			return null;
+			return new KIterator();
 		}
 
 		@Override
 		public Object[] toArray() {
 			// TODO 自動生成されたメソッド・スタブ
 			check();
-			return null;
+			Object[] o = new Object[size()];
+			Node curNode = nodes;
+			int offset = 0;
+			while ((curNode = curNode.next) != null) {
+				o[offset++] = curNode.k;
+			}
+			return o;
 		}
 
 		@Override
-		public <T> T[] toArray(T[] a) {
+		public <T> T[] toArray(T[] o) {
 			// TODO 自動生成されたメソッド・スタブ
 			check();
-			return null;
+			Node curNode = nodes;
+			int offset = 0;
+			while ((curNode = curNode.next) != null) {
+				o[offset++] = (T) curNode.k;
+			}
+			return o;
 		}
 
 		@Override
@@ -334,6 +368,334 @@ public class OrderTrustedMap<K, V> implements Map<K, V> {
 		protected int exceptedMod() {
 			// TODO 自動生成されたメソッド・スタブ
 			return exceptedMod;
+		}
+
+		class KIterator extends Check implements Iterator<K> {
+			Node curNode = nodes;
+
+			@Override
+			protected int exceptedMod() {
+				// TODO 自動生成されたメソッド・スタブ
+				return exceptedMod;
+			}
+
+			@Override
+			public boolean hasNext() {
+				// TODO 自動生成されたメソッド・スタブ
+				check();
+				return curNode.next != null;
+			}
+
+			@Override
+			public K next() {
+				// TODO 自動生成されたメソッド・スタブ
+				check();
+				return (curNode = curNode.next).k;
+			}
+
+			@Override
+			public void remove() {
+				// TODO 自動生成されたメソッド・スタブ
+				throw new UnsupportedOperationException();
+			}
+		}
+	}
+
+	final class Values extends Check implements Collection<V> {
+		int exceptedMod = curModified;
+
+		@Override
+		public int size() {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			return OrderTrustedMap.this.size();
+		}
+
+		@Override
+		public boolean isEmpty() {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			return OrderTrustedMap.this.isEmpty();
+		}
+
+		@Override
+		public boolean contains(Object o) {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			return OrderTrustedMap.this.containsValue(o);
+		}
+
+		@Override
+		public Iterator<V> iterator() {
+			// TODO 自動生成されたメソッド・スタブ
+			return new VIterator();
+		}
+
+		@Override
+		public Object[] toArray() {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			Object[] o = new Object[size()];
+			Node curNode = nodes;
+			int offset = 0;
+			while ((curNode = curNode.next) != null) {
+				o[offset++] = curNode.v;
+			}
+			return o;
+		}
+
+		@Override
+		public <T> T[] toArray(T[] o) {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			Node curNode = nodes;
+			int offset = 0;
+			while ((curNode = curNode.next) != null) {
+				o[offset++] = (T) curNode.v;
+			}
+			return o;
+		}
+
+		@Override
+		public boolean add(V e) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean remove(Object o) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean containsAll(Collection<?> c) {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			for (Object k : c)
+				if (!contains(k))
+					return false;
+			return true;
+		}
+
+		@Override
+		public boolean addAll(Collection<? extends V> c) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean removeAll(Collection<?> c) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean retainAll(Collection<?> c) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void clear() {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		protected int exceptedMod() {
+			// TODO 自動生成されたメソッド・スタブ
+			return exceptedMod;
+		}
+
+		class VIterator extends Check implements Iterator<V> {
+			Node curNode = nodes;
+
+			@Override
+			protected int exceptedMod() {
+				// TODO 自動生成されたメソッド・スタブ
+				return exceptedMod;
+			}
+
+			@Override
+			public boolean hasNext() {
+				// TODO 自動生成されたメソッド・スタブ
+				check();
+				return curNode.next != null;
+			}
+
+			@Override
+			public V next() {
+				// TODO 自動生成されたメソッド・スタブ
+				check();
+				return (curNode = curNode.next).v;
+			}
+
+			@Override
+			public void remove() {
+				// TODO 自動生成されたメソッド・スタブ
+				throw new UnsupportedOperationException();
+			}
+		}
+	}
+
+	final class EntrySet extends Check implements Set<Map.Entry<K, V>> {
+		int exceptedMod = curModified;
+
+		@Override
+		public int size() {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			return OrderTrustedMap.this.size();
+		}
+
+		@Override
+		public boolean isEmpty() {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			return OrderTrustedMap.this.isEmpty();
+		}
+
+		@Override
+		public boolean contains(Object o) {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			if (o instanceof Map.Entry) {
+				if (containsKey(((Map.Entry) o).getKey())) {
+					if (((Map.Entry) o).getValue() == null) {
+						if (get(((Map.Entry) o).getKey()) == null) {
+							return true;
+						}
+					} else {
+						if (get(((Map.Entry) o).getKey()) == null) {
+							return false;
+						}
+						if (get(((Map.Entry) o).getKey()) == ((Map.Entry) o).getValue()) {
+							return true;
+						}
+						if (get(((Map.Entry) o).getKey()).equals(((Map.Entry) o).getValue())) {
+							return true;
+						}
+					}
+				} else {
+					return false;
+				}
+			}
+			return false;
+		}
+
+		@Override
+		public Iterator<Map.Entry<K, V>> iterator() {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			return new KVIterator();
+		}
+
+		@Override
+		public Object[] toArray() {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			Object[] o = new Object[size()];
+			Node curNode = nodes;
+			int offset = 0;
+			while ((curNode = curNode.next) != null) {
+				o[offset++] = curNode.k;
+			}
+			return o;
+		}
+
+		@Override
+		public <T> T[] toArray(T[] o) {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			Node curNode = nodes;
+			int offset = 0;
+			while ((curNode = curNode.next) != null) {
+				o[offset++] = (T) curNode.k;
+			}
+			return o;
+		}
+
+		@Override
+		public boolean add(Map.Entry<K, V> e) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean remove(Object o) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean containsAll(Collection<?> c) {
+			// TODO 自動生成されたメソッド・スタブ
+			check();
+			for (Object k : c)
+				if (!contains(k))
+					return false;
+			return true;
+		}
+
+		@Override
+		public boolean addAll(Collection<? extends Map.Entry<K, V>> c) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean retainAll(Collection<?> c) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public boolean removeAll(Collection<?> c) {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public void clear() {
+			// TODO 自動生成されたメソッド・スタブ
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		protected int exceptedMod() {
+			// TODO 自動生成されたメソッド・スタブ
+			return exceptedMod;
+		}
+
+		class KVIterator extends Check implements Iterator<Map.Entry<K, V>> {
+			Node curNode = nodes;
+
+			@Override
+			protected int exceptedMod() {
+				// TODO 自動生成されたメソッド・スタブ
+				return exceptedMod;
+			}
+
+			@Override
+			public boolean hasNext() {
+				// TODO 自動生成されたメソッド・スタブ
+				check();
+				return curNode.next != null;
+			}
+
+			@Override
+			public Map.Entry<K, V> next() {
+				// TODO 自動生成されたメソッド・スタブ
+				check();
+				return (curNode = curNode.next).new ImmutableEntry();
+			}
+
+			@Override
+			public void remove() {
+				// TODO 自動生成されたメソッド・スタブ
+				throw new UnsupportedOperationException();
+			}
 		}
 	}
 }
